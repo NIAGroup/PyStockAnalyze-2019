5677d7a1ca6168da7f4b1a0f9ac4a45ea7cf870a
Lennard Streat <lennard.streat@intel.com>
2019-02-19 00:11:48 -0800

    Changes being made to script to support easy environment build upon n pulling the repository.

--- tools/build_env.sh
+++ tools/build_env.sh
@@ -1,9 +1,30 @@
 #!/bin/bash -e
 
-ROOT_DIR=$(git rev-parse --show-toplevel)
-VENV_NAME=pydjango-build-env
 
-python -m venv $ROOT_DIR/build/$VENV_NAME
-pip install -r $ROOT_DIR/tools/requirements.txt
+echo "[build] Creating project environment variables"
+PROJ_ROOT_DIR=$(git rev-parse --show-toplevel)
+PROJ_BUILD_PATH=$PROJ_ROOT_DIR/build
+PROJ_SRC_PATH=$PROJ_ROOT_DIR/src
+PROJ_TOOL_PATH=$PROJ_ROOT_DIR/tools
+
+PROJ_VENV_NAME=pydjango-build-env
+PROJ_VENV_PATH=$PROJ_BUILD_PATH/$PROJ_VENV_NAME
+PROJ_ACTENV_PATH=$PROJ_VENV_PATH/Scripts/activate
 
 # Create activate script pointer
+alias activate='source $PROJ_ACTENV_PATH'
+alias cleanenv='rm $PROJ_BUILD_PATH/* -rf; rm $PROJ_ROOT_PATH/activate; deactivate > /dev/null'
+
+if [ ! -d $PROJ_VENV_PATH ]; then
+	echo "[build] Creating python virtual environment"
+	python -m venv $PROJ_VENV_PATH
+	# Install dependencies into environment
+	echo "[build] Instsalling dependencies"
+	activate
+	python -m pip install --upgrade pip
+	pip install -r $PROJ_TOOL_PATH/requirements.txt $PROXY_OPT
+else
+	echo "[build] Python virtual environment already exists"
+fi
+
+
