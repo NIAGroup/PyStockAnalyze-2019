49a33a8980758f4f8dabd66e8680f93a5cf52b57
Princton Brennan <princtonbrennan@Princtons-MacBook-Pro.local>
2019-04-18 23:32:24 -0700

    files removed

--- src/pysdjango/pysdjango/urls.py
+++ src/pysdjango/pysdjango/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,5 +22,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('homepage_app.urls'), name='home_view'),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
