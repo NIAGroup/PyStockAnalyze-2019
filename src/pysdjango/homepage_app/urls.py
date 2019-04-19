7420406ffeb3504dcaa560d43f2b5970dfede868
princton1 <princtonb@gmail.com>
2019-04-18 19:05:57 -0700

    homepage_app viewer

---
+++ src/pysdjango/homepage_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from . import views
+
+app_name = "home"
+
+urlpatterns = [
+    path('', views.home, name=app_name),
+]
--- src/pysdjango/pysdjango/urls.py
+++ src/pysdjango/pysdjango/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('homepage_app.urls'), name='home_view'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
