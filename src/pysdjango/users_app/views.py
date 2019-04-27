6e58d8f3780658fadaba36983c12229280fd4443
Lennard Streat <lennard.streat@intel.com>
2019-04-27 14:27:40 -0700

    Updating the view with comments.

--- src/pysdjango/users_app/views.py
+++ src/pysdjango/users_app/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+"""	User Application View
+	Serves the HTTP response content to the requesting client.
+"""
 
+# =============================================================================
+# 1 | Document Metadata
+# =============================================================================
+__author__    = 'Lennard Streat'
+__copyright__ = 'Network of Intel African Americans 2019, PyStockAnalyze Project'
+__credits__   = ['Lennard Streat', 'Princton Brennan']
+__license__   = 'MIT'
+__version__   = '1.0'
+__email__     = 'nia.stem.club'
+
+# =============================================================================
+# 2 | Imports
+# =============================================================================
+# DJango imports
+from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
+# =============================================================================
+# 3 | Registration View
+# =============================================================================
 def register(request):
 	form = UserCreationForm()
 	return render(request, 'users/register.html', {'form' : form})
