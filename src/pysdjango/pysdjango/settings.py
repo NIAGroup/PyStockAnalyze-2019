04c650f05ddabde97bba4a3706a19a5600ef80d8
Lennard Streat <lennard.streat@intel.com>
2019-04-27 14:16:59 -0700

    Modifying views to include the user creation form.

--- src/pysdjango/pysdjango/settings.py
+++ src/pysdjango/pysdjango/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'homepage_app',
+    'users_app.apps.UsersAppConfig',
 ]
 
 MIDDLEWARE = [
--- src/pysdjango/users_app/views.py
+++ src/pysdjango/users_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+	form = UserCreationForm()
