4dbabab4603881af4e9404c9ae17f6ef0b2344f2
Lennard Streat <lennard.streat@intel.com>
2019-05-03 19:33:15 -0700

    Updating develop branch to now have base for stockview page.

--- src/pysdjango/pysdjango/settings.py
+++ src/pysdjango/pysdjango/settings.py
@@ -125,14 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#This is where we will keep all of our styling css files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'assets'),
-)
-
-
-
-#MEDIA_ROOT = os.path.join(parent_dir, 'media/')
-MEDIA_URL = '/media/'
--- src/pysdjango/stockview_app/urls.py
+++ src/pysdjango/stockview_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='stockview_app'
+
 urlpatterns = [
 	path('', views.home_fbv, name='home_fbv'),
 ]
