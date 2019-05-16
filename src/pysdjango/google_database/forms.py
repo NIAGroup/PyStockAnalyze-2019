a94f90dfe5e886ae8aeb13e25344e521897336f2
skasero <spencerkkr@gmail.com>
2019-05-15 21:58:01 -0700

    tried adding methods of pulling data from website, build does not work

--- src/pysdjango/google_database/forms.py
+++ src/pysdjango/google_database/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
-class SearchForm(forms.Form):
+class Search(forms.Form):
     search = forms.CharField(label='Search', max_length=20, required=False)
--- src/pysdjango/google_database/views.py
+++ src/pysdjango/google_database/views.py
@@ -2,26 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
-from .forms import SearchForm
 from .googleapi import CSEQuery
+from .forms import Search
 
 class Home(TemplateView):
-	template_name = 'google_database/index.html'
+    template_name = 'google_database/index.html'
 
-	def get(self, request):
-		inputText = SearchForm()
-		return render(request, self.template_name, {'inputText': inputText})
+    def get(self, request):
+        return render(request, self.template_name)
 
-	def post(self, request):
-		inputText = SearchForm(request.POST)
-		outputText = ""
-		if inputText.is_valid():
-			outputText = inputText.cleaned_data['post']
-			outputText = CSEQuery.cse_format(outputText)
-			inputText = SearchForm() # Reset form input
-		
-		args = {'form' : inputText, 'outputText' : outputText}
-		return render(request, self.template_name, args)
+    def post(self, request):
+        inputText = Search(request.POST)
+        inputText.search = request.POST.get('inputText')
+        outputText = "Empty"
+        if inputText.is_valid():
+            outputText = inputText.cleaned_data['search']
+            print(outputText)
+            outputText = CSEQuery.cse_format(outputText)
+            inputText = Search()  # Reset form input
 
-	def post_query_handler(request):
-		pass
+        args = {'inputText': inputText, 'outputText': outputText}
+        return render(request, self.template_name, args)
+
+    def post_query_handler(request):
+        pass
