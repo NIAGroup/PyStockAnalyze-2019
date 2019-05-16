fa651308d20803ca0219527d083e6d4ebb245352
skasero <spencerkkr@gmail.com>
2019-05-15 21:02:15 -0700

    changed some imports, now complies

--- src/pysdjango/google_database/views.py
+++ src/pysdjango/google_database/views.py
@@ -2,23 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
-from .forms import MainForm
+from .forms import SearchForm
 from .googleapi import CSEQuery
 
 class Home(TemplateView):
 	template_name = 'google_database/index.html'
 
 	def get(self, request):
-		inputText = MainForm()
+		inputText = SearchForm()
 		return render(request, self.template_name, {'inputText': inputText})
 
 	def post(self, request):
-		inputText = MainForm(request.POST)
+		inputText = SearchForm(request.POST)
 		outputText = ""
 		if inputText.is_valid():
 			outputText = inputText.cleaned_data['post']
 			outputText = CSEQuery.cse_format(outputText)
-			inputText = MainForm() # Reset form input
+			inputText = SearchForm() # Reset form input
 		
 		args = {'form' : inputText, 'outputText' : outputText}
 		return render(request, self.template_name, args)
